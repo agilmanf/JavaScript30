6e9e9088947f7637d7618459a17c2bbcb16f98c3
Gilman <agilmanf@gmail.com>
2021-09-01 09:21:10 +0700

    Updating user interface & adding some music

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,10 +1,58 @@
+// Music Player
+const song = document.querySelector('.song');
+const playButton = document.querySelector('.play-button');
+const slider = document.querySelector('.music-duration');
+let isPlaying = false;
+let duration;
+
+playButton.addEventListener('click',PlaySong);
+
+function PlaySong() {
+    if(!isPlaying) {
+        song.play();
+        isPlaying = true;
+        playButton.innerHTML = "Pause";
+        duration = song.duration;
+        slider.max = duration;
+        Crowd();
+    }
+    else {
+        song.pause();
+        isPlaying = false;
+        playButton.innerHTML = "Play";
+        Crowd();
+    }
+
+    slider.addEventListener('change',function(){
+        song.currentTime = slider.value;
+    })
+
+    song.addEventListener('timeupdate', function(){
+        slider.value = song.currentTime;
+    })
+}
+// Crowd Animation
+const crowd = document.querySelector('.crowd');
+function Crowd(){
+    crowd.classList.toggle('crowd-anim');
+
+    crowd.addEventListener('transitionend',function(){
+        crowd.classList.toggle('crowd-anim');
+    })
+    
+}
+
+
+//////////////////////////////////////
 function PlayAudio(e) {
     let key;
     let audio;
 
-    if(e.target.parentElement.classList.contains('key')){
+    //alert('a');
+
+    if(e.target.classList.contains('key')){
         // mouse pressed
-        const code = e.target.parentElement.getAttribute('data-key');
+        const code = e.target.getAttribute('data-key');
         key = document.querySelector(`.key[data-key="${code}"]`);
         audio = document.querySelector(`audio[data-key="${code}"]`);
     }
